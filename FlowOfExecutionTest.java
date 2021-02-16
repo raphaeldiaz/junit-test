fe0db727ba4bb3a6bb4cda0f52644b8f06dcbd7e
raphaeldiaz <72459750+raphaeldiaz@users.noreply.github.com>
2021-02-16 09:21:34 -0800

    Create FlowOfExecutionTest.java

---
+++ FlowOfExecutionTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FlowOfExecutionTest {
+
+	@Test
+	@DisplayName("Test overloaded method dobuleMe() for accurate string value.") 
+    public void testDoubleMeString() {
+		
+		String expected = "test test";
+		String actual = MyMethods.doubleMe("test");   // Given & When because public static void
+		
+		assertEquals(expected, actual);
+		
+		
+	}
+	
+    @Test
+    @DisplayName("Test overloaded method doubleMe() for accurate long value.")
+    public void testDoubleMeLong() {
+    	
+    	long expected = 20000000L;
+    	long actual   = MyMethods.doubleMe(10000000L);
+    	
+    	assertEquals(expected, actual);
+    }
+    
+    @Test
+    @DisplayName("Test overloaded method doubleMe() for accurate int value.")
+    public void testDoubleMeInt() {
+    
+      int expected = 2000;
+      int actual = MyMethods.doubleMe(1000);
+      
+      assertEquals(expected, actual);
+      }
